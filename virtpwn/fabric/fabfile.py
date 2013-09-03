1bbcfc50e6cf7c1a81ee56b82522f9d95b292470
Jakub Ruzicka <jruzicka@redhat.com>
2013-09-03 19:16:16 +0200

    Add update_hosts builtin provisioning task

--- virtpwn/fabric/fabfile.py
+++ virtpwn/fabric/fabfile.py
@@ -1,4 +1,6 @@
 from fabric.api import run, local, env, settings
+from fabric.utils import warn
+import re
 
 
 DEFAULT_PASSWORD = 'secret'
@@ -16,7 +18,27 @@ def ssh_copy_id(password=DEFAULT_PASSWORD, user=None):
     ensure_ssh_known_host(user)
     local("sshpass -p '%s' ssh-copy-id '%s@%s'" % (password, user, env.host))
 
+def update_hosts(name, old_name=None):
+    if not old_name:
+        old_name = run("hostname")
+    hosts = run("cat /etc/hosts")
+    m = re.search("^127\.0\.0\.1\s.*$", hosts, flags=re.M)
+    if not m:
+        run("echo '127.0.0.1    %s' >> /etc/hosts" % name)
+        return
+    line = m.group(0)
+    if re.search('\s%s[\s$]' % name, line):
+        return
+    if old_name and old_name != name and old_name != 'localhost' \
+       and re.search("\s%s[\s$]" % old_name, line):
+        # replace old hostname with new one
+        run("sed -i -r 's/(\s)%s(\s|$)/\1%s\2/g' /etc/hosts" % (old_name, name))
+    else:
+        # append new hostname
+        run("sed -i -r '/^127\.0\.0\.1|::1/s#$# %s#' /etc/hosts" % name)
+
 def hostname(name):
+    update_hosts(name)
     run("echo '%s' > /etc/hostname" % name)
     run("hostname '%s'" % name)
 
